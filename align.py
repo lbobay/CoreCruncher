76f168d2d9a125cf4e282e4c3f6b5aea3223d193
Louis-Marie Bobay <57679670+lbobay@users.noreply.github.com>
2023-05-15 16:40:27 -0400

    Add files via upload

--- align.py
+++ align.py
@@ -1,6 +1,7 @@
 
 import os
 import sys
+import subprocess
 
 ALIGN = sys.argv[-3]
 out_path = sys.argv[-2]
@@ -8,12 +9,21 @@ ext = sys.argv[-1]
 
 FILES = os.listdir(out_path + "core")
 
+
+
+
 stuff = ALIGN.lower()
 if "muscle" in stuff:
+	version = subprocess.check_output("muscle -version", shell=True)
+	version = version.lower()
+	sub = int(version.split(".")[0][-1])
 	print("Calling muscle with '",ALIGN,"' with default parameters")
 	for seq in FILES:
 		if seq.endswith(ext):
-			os.system(ALIGN + " -in " + out_path + "core/" + seq + "  -out " + out_path + "core/" + seq + ".align")
+			if sub <= 4:
+				os.system(ALIGN + " -in " + out_path + "core/" + seq + "  -out " + out_path + "core/" + seq + ".align")
+			else:
+				os.system(ALIGN + " -align " + out_path + "core/" + seq + "  -output " + out_path + "core/" + seq + ".align")
 elif "mafft" in stuff:
 	print("Calling mafft with '",ALIGN,"' with default parameters")
 	for seq in FILES:

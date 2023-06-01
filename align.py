56f02f584089e04dc5543288f109b42a074d9bef
Louis-Marie Bobay <57679670+lbobay@users.noreply.github.com>
2023-06-01 13:22:45 -0400

    Add files via upload

--- align.py
+++ align.py
@@ -16,6 +16,7 @@ stuff = ALIGN.lower()
 if "muscle" in stuff:
 	version = subprocess.check_output("muscle -version", shell=True)
 	version = version.lower()
+	version = version.strip("v")
 	sub = int(version.split(".")[0][-1])
 	print("Calling muscle with '",ALIGN,"' with default parameters")
 	for seq in FILES:
